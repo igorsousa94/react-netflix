9ce66b461b759da1046c7bbee0c404257d56a2d1
Igor Santos <contatoigorsousa@gmail.com>
2020-12-12 22:25:12 -0300

    task - voltando dependencias

--- android/app/src/main/java/netflix-react-native/MainApplication.java
+++ android/app/src/main/java/netflix-react-native/MainApplication.java
@@ -20,8 +20,7 @@ import org.unimodules.core.interfaces.SingletonModule;
 import expo.modules.constants.ConstantsPackage;
 import expo.modules.permissions.PermissionsPackage;
 import expo.modules.filesystem.FileSystemPackage;
-
-import com.microsoft.codepush.react.CodePush;
+import expo.modules.updates.UpdatesController;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
@@ -53,15 +52,22 @@ public class MainApplication extends Application implements ReactApplication {
     }
 
     @Override
-    protected String getJSBundleFile() {
-        return CodePush.getJSBundleFile();
+    protected @Nullable String getJSBundleFile() {
+      if (BuildConfig.DEBUG) {
+        return super.getJSBundleFile();
+      } else {
+        return UpdatesController.getInstance().getLaunchAssetFile();
+      }
     }
 
     @Override
     protected @Nullable String getBundleAssetName() {
-      return super.getBundleAssetName();
+      if (BuildConfig.DEBUG) {
+        return super.getBundleAssetName();
+      } else {
+        return UpdatesController.getInstance().getBundleAssetName();
+      }
     }
-    
   };
 
   @Override
@@ -74,6 +80,10 @@ public class MainApplication extends Application implements ReactApplication {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
     initializeFlipper(this); // Remove this line if you don't want Flipper enabled
+
+    if (!BuildConfig.DEBUG) {
+      UpdatesController.initialize(this);
+    }
   }
 
   /**
